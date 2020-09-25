0ba5f5d89382cb7dc17fa714494780e5833464a4
AdamManselll <adammansellhmb@gmail.com>
2020-09-25 10:22:57 -0700

    Fail Spec 1

--- lib/ana_anti.rb
+++ lib/ana_anti.rb
@@ -2,30 +2,24 @@ require('pry')
 
 def anagramFunc(word1, word2)
 
+# word 1
 puts "VALUE:" << word1
 puts "LENGTH:" << String(word1.length)
-
 wordArray1 = []
-wordArray2 = []
+wordArray1.push(word1.chars.sort.join)
+puts wordArray1
 
-for i in 0..word1.length - 1
-  puts "CHAR FORWARD:" << word1[i]
-  wordArray1.push(word1[i])
-end
-
+# word 2
 puts "VALUE:" << word2
 puts "LENGTH:" << String(word2.length)
-
-for i in 0..word2.length - 1
-  puts "CHAR FORWARD:" << word2[i]
-  wordArray2.push(word2[i])
-end
-
-puts wordArray1
+wordArray2 = []
+wordArray2.push(word2.chars.sort.join)
 puts wordArray2
 
+# check if equal
+wordArray1 == wordArray2 ? (return "These words are anagrams!" ) : (return "These words are not anagrams!")
 
 
-  puts("These words are anagrams!")
-  return "These words are anagrams!"
+  # puts("These words are anagrams!")
+  # return "These words are anagrams!"
 end
--- spec/ana_anti_spec.rb
+++ spec/ana_anti_spec.rb
@@ -3,6 +3,6 @@ require ('ana_anti')
 
 describe("#anagramFunc") do
   it ("it reads words and checks for anagrams") do
-    expect(anagramFunc("dog", "god")).to(eq("These words are anagrams!"))
+    expect(anagramFunc("me", "god")).to(eq("These words are anagrams!"))
   end
 end

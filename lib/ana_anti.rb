4594579dafb6a57ea68b25a8753703a645e2fb32
AdamManselll <adammansellhmb@gmail.com>
2020-09-25 11:35:08 -0700

    Fail Spec 2

--- lib/ana_anti.rb
+++ lib/ana_anti.rb
@@ -5,21 +5,19 @@ def anagramFunc(word1, word2)
 # word 1
 puts "VALUE:" << word1
 puts "LENGTH:" << String(word1.length)
-wordArray1 = []
-wordArray1.push(word1.downcase.chars.sort.join)
-puts wordArray1
+charCheck1 = word1.downcase.chars.sort.join
+puts charCheck1
 
 # word 2
 puts "VALUE:" << word2
 puts "LENGTH:" << String(word2.length)
-wordArray2 = []
-wordArray2.push(word2.downcase.chars.sort.join)
-puts wordArray2
+charCheck2 = word2.downcase.chars.sort.join
+puts charCheck2
 
 # check if equal
-# antigramTest = (wordArray1.match(wordArray2) == false ? (return "These words have no letter matches and are antigrams.") : (return "It got nothing"))
-# anagramTest = (wordArray1 == wordArray2 ? (return "These words are anagrams!" ) : antigramTest)
-(wordArray1.scan(/aeiou/) && wordArray2.scan(/aeiou/)) ? (wordArray1 == wordArray2 ? (return "These words are anagrams!" ) : (wordArray1.match(wordArray2) == false ? (return "These words have no letter matches and are antigrams.") : (return "It got nothing"))) : (return "You need to input actual words!")
+# antigramTest = (charCheck1.include?(charCheck2) == false ? (return "These words have no letter matches and are antigrams.") : (return "It got nothing"))
+# anagramTest = (charCheck1 == charCheck2 ? (return "These words are anagrams!" ) : antigramTest)
+((charCheck1.match(/[aeiou]/)) && (charCheck2.match(/[aeiou]/))) ? (charCheck1 == charCheck2 ? (return "These words are anagrams!" ) : (charCheck1.include?(charCheck2) ? (return "It got nothing") : (return "These words have no letter matches and are antigrams."))) : (return "You need to input actual words!")
 
 
 
--- spec/ana_anti_spec.rb
+++ spec/ana_anti_spec.rb
@@ -3,6 +3,9 @@ require ('ana_anti')
 
 describe("#anagramFunc") do
   it ("it reads words and checks for anagrams") do
-    expect(anagramFunc("Tea", "Eat")).to(eq("These words are anagrams!"))
+    expect(anagramFunc("dog", "god")).to(eq("These words are anagrams!"))
+  end
+  it ("it reads words and checks for antigrams") do
+    expect(anagramFunc("hi", "bye")).to(eq("These words are anagrams!"))
   end
 end

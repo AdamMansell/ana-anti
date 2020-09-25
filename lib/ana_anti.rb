f4393faf937df1091d8a34c8fa91ba16369d97b7
AdamManselll <adammansellhmb@gmail.com>
2020-09-25 10:54:53 -0700

    Update ana_anti.rb to confirm that they are real words

--- lib/ana_anti.rb
+++ lib/ana_anti.rb
@@ -6,18 +6,21 @@ def anagramFunc(word1, word2)
 puts "VALUE:" << word1
 puts "LENGTH:" << String(word1.length)
 wordArray1 = []
-wordArray1.push(word1.chars.sort.join)
+wordArray1.push(word1.downcase.chars.sort.join)
 puts wordArray1
 
 # word 2
 puts "VALUE:" << word2
 puts "LENGTH:" << String(word2.length)
 wordArray2 = []
-wordArray2.push(word2.chars.sort.join)
+wordArray2.push(word2.downcase.chars.sort.join)
 puts wordArray2
 
 # check if equal
-wordArray1 == wordArray2 ? (return "These words are anagrams!" ) : (return "These words are not anagrams!")
+# antigramTest = (wordArray1.match(wordArray2) == false ? (return "These words have no letter matches and are antigrams.") : (return "It got nothing"))
+# anagramTest = (wordArray1 == wordArray2 ? (return "These words are anagrams!" ) : antigramTest)
+(wordArray1.scan(/aeiou/) && wordArray2.scan(/aeiou/)) ? (wordArray1 == wordArray2 ? (return "These words are anagrams!" ) : (wordArray1.match(wordArray2) == false ? (return "These words have no letter matches and are antigrams.") : (return "It got nothing"))) : (return "You need to input actual words!")
+
 
 
   # puts("These words are anagrams!")
